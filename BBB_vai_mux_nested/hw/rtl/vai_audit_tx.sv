9cad0f6110c9c45512b14489faa1be0eb54b18b5
Jiacheng Ma <jcma@umich.edu>
2019-04-04 23:40:31 +0800

    hijack for 2M pages

--- BBB_vai_mux_nested/hw/rtl/vai_audit_tx.sv
+++ BBB_vai_mux_nested/hw/rtl/vai_audit_tx.sv
@@ -119,12 +119,12 @@ module vai_audit_tx #(parameter NUM_SUB_AFUS=8)
             always_comb
             begin
                 up_TxPort[n].c0 = T2_c0;
-                up_TxPort[n].c0.hdr.address[33:30] = T2_c0.hdr.address[9:6];
-                up_TxPort[n].c0.hdr.address[9:6] = T2_c0.hdr.address[33:30];;
+                up_TxPort[n].c0.hdr.address[33:30] = T2_c0.hdr.address[18:15];
+                up_TxPort[n].c0.hdr.address[18:15] = T2_c0.hdr.address[33:30];;
 
                 up_TxPort[n].c1 = T2_c1;
-                up_TxPort[n].c1.hdr.address[33:30] = T2_c1.hdr.address[9:6];
-                up_TxPort[n].c1.hdr.address[9:6] = T2_c1.hdr.address[33:30];
+                up_TxPort[n].c1.hdr.address[33:30] = T2_c1.hdr.address[18:15];
+                up_TxPort[n].c1.hdr.address[18:15] = T2_c1.hdr.address[33:30];
 
                 up_TxPort[n].c2 = T2_c2;
             end
